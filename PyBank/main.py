26343c17ffd0456753fdb6002dae2257dd6b7581
nvanlangen <59780892+nvanlangen@users.noreply.github.com>
2020-03-16 19:22:03 -0400

    Modified the output file code to no longer use csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,8 +20,8 @@ with open(csvpath,"r") as csvfile:
     #Set up file reader with , delimiter
     csvreader = csv.reader(csvfile, delimiter=',')
     
-    #Read header and ignore
-    next(csvreader)
+    #Read header and save in variable
+    pl_header = next(csvreader)
 
     #Read each row in file
     for row in csvreader:
@@ -44,8 +44,8 @@ pl_diff_list.pop(0)
 print("Financial Analysis")
 print("----------------------------")
 
-#Total Months length of the profit/loss list
-print(f"Total Months: {len(profit_loss_list)}")
+#Total Months length of the month list
+print(f"Total Months: {len(month_list)}")
 
 #Total sum of the profit/loss list
 print(f"Total: ${sum(profit_loss_list)}")
@@ -59,15 +59,14 @@ print(f"Greatest Increase in Profits: {month_list[pl_diff_list.index(max(pl_diff
 #Greatest Decrease is min of profit/loss difference list, month uses index of the min value + 1 to account for the first month being removed
 print(f"Greatest Decrease in Profits: {month_list[pl_diff_list.index(min(pl_diff_list))+1]} (${min(pl_diff_list)})")
 
-#Open output file, set newline for Windows 
-with open(analysispath,"w",newline='') as textfile:
-
-    #Write lines to the file, same as was printed on the console
-    textwriter = csv.writer(textfile)
-    textwriter.writerow(["Financial Analysis"])
-    textwriter.writerow(["----------------------------"])
-    textwriter.writerow(["Total Months: " + str(len(profit_loss_list))])
-    textwriter.writerow(["Total: $" + str(sum(profit_loss_list))])
-    textwriter.writerow(["Average  Change: $" + str(round(sum(pl_diff_list) / len(pl_diff_list),2))])
-    textwriter.writerow(["Greatest Increase in Profits: " + month_list[pl_diff_list.index(max(pl_diff_list))+1] + "($" + str(max(pl_diff_list)) + ")"])
-    textwriter.writerow(["Greatest Decrease in Profits: " + month_list[pl_diff_list.index(min(pl_diff_list))+1] + "($" + str(min(pl_diff_list)) + ")"])
+#Open output file
+with open(analysispath,"w") as analysis_file:
+    #Write lines to the text file, same as was printed on the console, not using csv for output as it is not comma separated data
+    analysis_file.write("Financial Analysis\n")
+    analysis_file.write("----------------------------\n")
+    analysis_file.write("Total Months: " + str(len(profit_loss_list))+"\n")
+    analysis_file.write("Total: $" + str(sum(profit_loss_list))+"\n")
+    analysis_file.write("Average  Change: $" + str(round(sum(pl_diff_list) / len(pl_diff_list),2))+"\n")
+    analysis_file.write("Greatest Increase in Profits: " + month_list[pl_diff_list.index(max(pl_diff_list))+1] + "($" + str(max(pl_diff_list)) + ")\n")
+    analysis_file.write("Greatest Decrease in Profits: " + month_list[pl_diff_list.index(min(pl_diff_list))+1] + "($" + str(min(pl_diff_list)) + ")\n")
+    analysis_file.close()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-challenge
 Python Homework
+
+Financial Analysis
+
+Did not use the csv library for the output file because the data was not comma separated.  In initial testing, I tried the csv for output and the output was the same.
+
