3a7af989ce05b07974d1a6cb6ce6b134a47d3303
nvanlangen <59780892+nvanlangen@users.noreply.github.com>
2020-03-17 16:40:36 -0400

    Changed to use single function for output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,10 @@
 #Modules
 import os
 import csv
+import sys
 
-#Set up file path and names
+#Set up input file path and name
 csvpath = os.path.join('.', 'Resources', 'election_data.csv')
-resultspath = os.path.join('.', 'Resources', 'results.txt')
 
 #Define lists used
 candidate_list = []
@@ -30,38 +30,40 @@ with open(csvpath,"r") as csvfile:
             candidate_list.append(row[2])
             candidate_votes_list.append(1)
 
-#Get the sum of votes for all candidates
-total_votes = sum(candidate_votes_list)
+#Function to write output, parameter is file or terminal
+def write_results(output_mode):
+    if output_mode=="file":
+        #Write output to a file
+        orig_stdout = sys.stdout
+        resultspath = os.path.join('.', 'Resources', 'results.txt')
+        results_file = open(resultspath,"w")
+        sys.stdout = results_file
+        
+    #Get the sum of votes for all candidates
+    total_votes = sum(candidate_votes_list)
 
-#Set index for loop to 0
-candidate_index=0
+    #Set index for loop to 0
+    candidate_index=0
 
-#Print Election Results to console
-print("Election Results")
-print("-------------------------")
-print(f"Total Votes: {total_votes}")
-print("-------------------------")
-#Calculate percentage and display the Candidate, Percentage and Numbr of Votes
-for candidate in candidate_list:
-    print(f"{candidate}: {candidate_votes_list[candidate_index] / total_votes *100:.3f}% ({candidate_votes_list[candidate_index]})")
-    candidate_index += 1
-print("-------------------------")
-#Identify the Winner by finding max number of votes and using the index to get the name
-print(f"Winner: {candidate_list[candidate_votes_list.index(max(candidate_votes_list))]}")
-print("-------------------------")
-
-#Repeat above to write to a text file
-candidate_index=0
-#Open output file
-with open(resultspath,"w") as results_file:
-    results_file.write("Election Results\n")
-    results_file.write("-------------------------\n")
-    results_file.write(f"Total Votes: {total_votes}\n")
-    results_file.write("-------------------------\n")
+    #Print Election Results
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------")
+    #Calculate percentage and display the Candidate, Percentage and Numbr of Votes
     for candidate in candidate_list:
-        results_file.write(f"{candidate}: {candidate_votes_list[candidate_index] / total_votes *100:.3f}% ({candidate_votes_list[candidate_index]})\n")
+        print(f"{candidate}: {candidate_votes_list[candidate_index] / total_votes *100:.3f}% ({candidate_votes_list[candidate_index]})")
         candidate_index += 1
-    results_file.write("-------------------------\n")
-    results_file.write(f"Winner: {candidate_list[candidate_votes_list.index(max(candidate_votes_list))]}\n")
-    results_file.write("-------------------------\n")
-    results_file.close()
+    print("-------------------------")
+    #Identify the Winner by finding max number of votes and using the index to get the name
+    print(f"Winner: {candidate_list[candidate_votes_list.index(max(candidate_votes_list))]}")
+    print("-------------------------")
+
+    #Close the file and reset the stdout to the terminal
+    if output_mode=="file":
+        results_file.close()
+        sys.stdout = orig_stdout
+
+#Call function to write the file and again for the terminal
+write_results("file")
+write_results("terminal")
